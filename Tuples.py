4f75f70ebb72b650a602c95b23c5d6422b7847b7
SamanthaFaith <115935667+ChelangatFaith@users.noreply.github.com>
2023-01-19 14:45:24 +0300

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,18 @@
+#Tuples are like list 
+#They are indexed 
+#Allow duplication
+#The are unchangable
+#Uses parenthesis to create tuples
+#Allow all data types
+
+Tuple_1 = (1,2,3,4,"Samantha",True)
+print(Tuple_1) 
+
+
+#Creating tuple with single element
+tuple_2=("Samantha")
+print (tuple)
+
+#Accessing the elements in tuple
+key_tuple = (71,(12),45,89890.0987,456)
+ 
